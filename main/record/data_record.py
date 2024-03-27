009ee0a05985007ea1c399185b767e1682c32402
Infinity0901 <147404427+Infinity0901@users.noreply.github.com>
2024-03-27 10:56:38 +0100

    main

---
+++ main/record/data_record.py
@@ -0,0 +1,6 @@
+from muselsl import record
+
+if __name__ == '__main__':
+    record(6, filename='record/Test/Droite1.csv')
+    
+# Record for 6 s into a csv file wrote in : record/Test/Droite1.csv
